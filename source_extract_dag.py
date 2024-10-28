c66b917d1a9407673f73b8068190e6ad878eb96d
god-of-thunder <izumiwang11@gmail.com>
2024-10-28 15:28:24 +0800

    change to use pymongo package and add transaction

--- source_extract_dag.py
+++ source_extract_dag.py
@@ -1,14 +1,15 @@
 import os 
-import csv
 import logging
 import pandas as pd
-import subprocess
+from pymongo import MongoClient
 import boto3
 from datetime import datetime,timedelta
 from airflow import DAG
 from airflow.models import Variable
 from airflow.operators.python_operator import PythonOperator
 from airflow.hooks.base_hook import BaseHook
+import warnings,traceback
+warnings.filterwarnings('ignore')
 
 default_args = {
     "owner":"airflow",
@@ -31,87 +32,67 @@ def ETL_sample(**context):
     if len(os.listdir("/tmp"))>0:
         column_format_list = Variable.get("drop_column_list", deserialize_json=True)
         column_replacing_pair = context["dag_run"].conf.get("column_replacing_pair")
-        count_int = 0
+        
+        df = pd.read_csv("/tmp/file.csv",encoding="utf-8",engine='python')
+        counts = len(df.to_dict("records"))
+        logging.info('{0} {1}'.format("資料上傳的資料量",counts))
+        df = df.rename(columns=lambda x: x.strip())
+        df = df.rename(columns=lambda c: column_replacing_pair[c] if c in column_replacing_pair.keys() else c)
+        column_name_list = df.columns.values.tolist()
+        set(column_name_list) & set(column_format_list)==set(column_format_list)
+        df["date"] = pd.to_datetime(df["date"])        
+        df["date"] = pd.Series(
+                    df["date"]
+                    .dt.tz_localize("Etc/GMT-8")
+                    .dt.tz_convert("UTC")
+                    .dt.tz_localize(None)
+                    .dt.to_pydatetime(),
+                    dtype=object,
+                )
+        df["value"] = pd.to_numeric(df["value"], "coerce")
+        df["description"] = df["description"].astype(str).apply(lambda s:s.strip()) + "-" + df["sample need to concatenate"].astype(str)
+        df = df.drop(Variable.get("drop_column_list", deserialize_json=True), axis=1)
+        records = df.to_dict(orient='records')
+
+        # load to mongodb
+        conn = BaseHook.get_connection('demo_db')
+        client = MongoClient(
+        host = conn.host,
+        username = conn.login,
+        password = conn.password,
+        port = conn.port,
+        authMechanism='SCRAM-SHA-256'
+        )
+        session = client.start_session()
+        db = client[conn.schema]
+        collection = db[ Variable.get("insert_collection")]
+        collection.insert_many(records)
         try:
-            df = pd.read_csv("/tmp/file.csv",encoding="utf-8",engine='python')
-            counts = len(df.to_dict("records"))
-            logging.info('{0} {1}'.format("資料上傳的資料量",counts))
-            df = df.rename(columns=lambda x: x.strip())
-            df = df.rename(columns=lambda c: column_replacing_pair[c] if c in column_replacing_pair.keys() else c)
-            column_name_list = df.columns.values.tolist()
-            set(column_name_list) & set(column_format_list)==set(column_format_list)
-            df["date"] = pd.to_datetime(df["date"])        
-            df["date"] = pd.Series(
-                        df["date"]
-                        .dt.tz_localize("Etc/GMT-8")
-                        .dt.tz_convert("UTC")
-                        .dt.tz_localize(None)
-                        .dt.to_pydatetime(),
-                        dtype=object,
-                    )
-            df["value"] = pd.to_numeric(df["value"], "coerce")
-            df["description"] = df["description"].astype(str).apply(lambda s:s.strip()) + "-" + df["sample need to concatenate"].astype(str)
-            df = df.drop(Variable.get("drop_column_list", deserialize_json=True), axis=1)
+            with session.start_transaction():
+                for record in records:
+                    collection.insert_one(record, session=session)
+            ok_counts = len(records)
+            logging.info('{0} {1}'.format("檔名,欄位名稱及編碼正確的資料量",ok_counts))
+            os.remove("/tmp/file.csv")
+        except:
+            message = traceback.format_exc()
+            error_message = f"""error : {message}"""
+            logging.error('交易失敗, 操作回滾復原') 
+            logging.error(error_message)     
+                                             
+        
 
-            import_df = df.rename(
-                columns={
-                    "name": "name.string()",
-                    "description": "result.string()",
-                    "value": "value.double()",
-                    "date": "time.date(2006-01-02 15:04:05)",
-                }
-            )
-            # use mongoimport to load file
-            conn = BaseHook.get_connection('demo_db')
-            collection = "your insert mongodb collection"
-            mongoimport_process = subprocess.Popen(
-                    [
-                    "mongoimport",
-                    # Auth
-                    f"--host={conn.host}",
-                    f"--port={conn.port}",
-                    f"--username={conn.login}",
-                    f"--password={conn.password}",
-                    f"--authenticationDatabase={conn.schema}",
-                    # # setting
-                    f"--db={conn.schema}",
-                    f"--collection={collection}",
-                    "--type=csv",
-                    "--headerline",
-                    "--ignoreBlanks",
-                    "--parseGrace=skipField",
-                    "--columnsHaveTypes",
-                    ],
-                stdout=subprocess.PIPE,
-                stdin=subprocess.PIPE,
-                stderr=subprocess.PIPE,
-            )
-            mongoimport_process.communicate(
-                input=import_df.to_csv(
-                    index=False,
-                    quoting=csv.QUOTE_NONNUMERIC,
-                    encoding="utf-8",
-                    date_format="%Y-%m-%d %H:%M:%S",
-                ).encode()
-            )
-            count_int+=1
-            ok_counts = len(import_df.to_dict("records"))
-            logging.info('{0} {1} {2}'.format(count_int,"檔名,欄位名稱及編碼正確的資料量",ok_counts)) 
-            if mongoimport_process.returncode != 0:
-                raise RuntimeError                                 
-        except:           
-            logging.info('{0} {1}'.format("tmp.csv","檔案編碼錯誤")) 
-            
-
-with DAG("ETL_sample", catchup=False, default_args=default_args,schedule_interval='* */1 * * *') as dag:
+with DAG("ETL_sample", catchup=False, default_args=default_args,schedule_interval='0 5 * * *') as dag:
     download_task = PythonOperator(
         task_id = "download_from_s3",
         python_callable = download_from_s3,
-        provide_context = True
+        provide_context = True,
+        dag = dag
     )
     ETL_process = PythonOperator(
         task_id = "ETL_sample",
         python_callable = ETL_sample,
+        provide_context = True,
         dag = dag
     )
     
